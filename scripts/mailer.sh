5ed786b125ae0055668dc01bfa82497328ad5116
atrox3d <atrox3d@gmail.com>
2020-03-25 10:18:10 +0100

    fix mailer windows

--- scripts/mailer.sh
+++ scripts/mailer.sh
@@ -1,1 +1,12 @@
-sendemail -l email.log     -f "sender@domain.com"      -u "Email Subject 2"      -t "receiver@domain.com"      -cc "receiver2@domain.com"      -bcc "receiver3@domain.com"      -s "smtp.***REMOVED***:587"      -o tls=yes      -xu "youremail@***REMOVED***"  echo mailbody | sendemail -l email.log     -f "***REMOVED***"      -u "Email Subject"      -t "***REMOVED***"  -s "smtp.***REMOVED***:587"      -o tls=yes      -xu "***REMOVED***" -xp "0Xp0rc0d10"
+#!/bin/bash
+#    -cc "receiver2@domain.com"  \
+#    -bcc "receiver3@domain.com"  \
+
+echo "hello" | sendemail -l email.log \
+    -f "***REMOVED***"  \
+    -u "test"  \
+    -t "***REMOVED***"  \
+    -s "smtp.***REMOVED***:587"  \
+    -o tls=yes  \
+    -xu "***REMOVED***"  \
+    -xp "0Xp0rc0d10" 
