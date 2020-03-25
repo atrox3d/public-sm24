6107546e9007c7ac85e368bbc18f14b1aa11d72f
atrox3d <atrox3d@gmail.com>
2020-03-25 17:07:09 +0100

    uppercase vars

--- scripts/mailer.sh
+++ scripts/mailer.sh
@@ -3,14 +3,14 @@ MAILLOG=log/sendemail.log
 
 [ $# -ge 3 ] || { echo "syntax: $0 address subject message [[attachment]...]"; exit 1; }
 
-mailto="$1"
-subject="$2"
-message="$3"
-smtpserver=smtp.***REMOVED***
-smtpport=587
-mailaddress=***REMOVED***
-mailuser=***REMOVED***
-mailpassword=0Xp0rc0d10
+MAILTO="$1"
+SUBJECT="$2"
+MESSAGE="$3"
+SMTPSERVER=smtp.***REMOVED***
+SMTPPORT=587
+MAILADDRESS=***REMOVED***
+MAILUSER=***REMOVED***
+MAILPASSWORD=0Xp0rc0d10
 
 if [ $# -ge 4 ]
 then
@@ -27,38 +27,38 @@ then
 	echo ATTACHMENTS:$ATTACHMENTS
 	#echo
 	sendemail \
-		-s  "${smtpserver}:${smtpport}" \
-		-xu "$mailuser" \
-		-xp "$mailpassword"  \
-		-f  "$mailaddress" \
-		-t  "$mailto" \
-		-u  "$subject" \
-		-m  "$message" \
+		-s  "${SMTPSERVER}:${SMTPPORT}" \
+		-xu "$MAILUSER" \
+		-xp "$MAILPASSWORD"  \
+		-f  "$MAILADDRESS" \
+		-t  "$MAILTO" \
+		-u  "$SUBJECT" \
+		-m  "$MESSAGE" \
 		-l  "$MAILLOG" \
 		-a ${ATTACHMENTS}
 else
 	sendemail \
-	-s  "${smtpserver}:${smtpport}" \
-	-xu "$mailuser" \
-	-xp "$mailpassword"  \
-	-f  "$mailaddress" \
-	-t  "$mailto" \
-	-u  "$subject" \
-	-m  "$message" \
+	-s  "${SMTPSERVER}:${SMTPPORT}" \
+	-xu "$MAILUSER" \
+	-xp "$MAILPASSWORD"  \
+	-f  "$MAILADDRESS" \
+	-t  "$MAILTO" \
+	-u  "$SUBJECT" \
+	-m  "$MESSAGE" \
 	-l  "$MAILLOG"
 fi
  
-exit
 
-
-#    -cc "receiver2@domain.com"  \
-#    -bcc "receiver3@domain.com"  \
-
-echo "hello" | sendemail -l email.log \
-    -f "***REMOVED***"  \
-    -u "test"  \
-    -t "***REMOVED***"  \
-    -s "smtp.***REMOVED***:587"  \
-    -o tls=yes  \
-    -xu "***REMOVED***"  \
-    -xp "0Xp0rc0d10" 
+#exit
+#
+##    -cc "receiver2@domain.com"  \
+##    -bcc "receiver3@domain.com"  \
+#
+#echo "hello" | sendemail -l email.log \
+#    -f "***REMOVED***"  \
+#    -u "test"  \
+#    -t "***REMOVED***"  \
+#    -s "smtp.***REMOVED***:587"  \
+#    -o tls=yes  \
+#    -xu "***REMOVED***"  \
+#    -xp "0Xp0rc0d10" 
