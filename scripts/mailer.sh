ed8407473d01e04971eb47cb8f12f1ce66244954
atrox3d <atrox3d@gmail.com>
2020-03-25 11:01:11 +0100

    mailer.sh update

--- scripts/mailer.sh
+++ scripts/mailer.sh
@@ -1,4 +1,56 @@
 #!/bin/bash
+MAILLOG=log/sendemail.log
+
+[ $# -ge 3 ] || { echo "syntax: $0 address subject message [[attachment]...]"; exit 1; }
+
+mailto="$1"
+subject="$2"
+message="$3"
+smtpserver=smtp.***REMOVED***
+smtpport=587
+mailaddress=***REMOVED***
+mailuser=***REMOVED***
+mailpassword=0Xp0rc0d10
+
+if [ $# -ge 4 ]
+then
+	shift
+	shift
+	shift
+	echo found $# attachments $*
+
+	for attachment in $*
+	do
+		echo check attachment -a -$attachment
+		ATTACHMENTS="$ATTACHMENTS -a $attachment"
+	done
+	echo ATTACHMENTS:$ATTACHMENTS
+	#echo \
+	sendemail \
+		-s  "${smtpserver}:${smtpport}" \
+		-xu "$mailuser" \
+		-xp "$mailpassword"  \
+		-f  "$mailaddress" \
+		-t  "$mailto" \
+		-u  "$subject" \
+		-m  "$message" \
+		-l  "$MAILLOG" \
+		${ATTACHMENTS}
+else
+	sendemail \
+	-s  "${smtpserver}:${smtpport}" \
+	-xu "$mailuser" \
+	-xp "$mailpassword"  \
+	-f  "$mailaddress" \
+	-t  "$mailto" \
+	-u  "$subject" \
+	-m  "$message" \
+	-l  "$MAILLOG"
+fi
+ 
+exit
+
+
 #    -cc "receiver2@domain.com"  \
 #    -bcc "receiver3@domain.com"  \
 
