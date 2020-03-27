cfb391cd512c8edd3852d9715da16076ade77889
atrox3d <atrox3d@gmail.com>
2020-03-27 20:07:53 +0100

    FORSE FORSE

--- scripts/firefox.check.sh
+++ scripts/firefox.check.sh
@@ -53,7 +53,11 @@ function firefoxcheck()
 	########################################################################################################
 	{
 	echo -e "########################################################################################################"
-	echo -e "$(timestamp)\tlocationID:$LOCATIONID\tstoreID:$STOREID\tbearerID:$BEARERID"
+	echo -e "$(timestamp)\tlocationID   : $LOCATIONID"
+	echo -e "$(timestamp)\tstoreID      : $STOREID"
+	echo -e "$(timestamp)\tbearerID     : $BEARERID"
+	echo -e "$(timestamp)\tstoreName    : $STORENAME"
+	echo -e "$(timestamp)\tstoreAddress : $STOREADDRESS"
 	echo -e "########################################################################################################"
 	} | tee -a $OUT_ERR >>$OUT_CHK 
 	#read
@@ -63,20 +67,33 @@ function firefoxcheck()
 	[ -v DEBUG ] && {
 		echo "$DEBUG \\"
 	}
+	#echo "curl 'https://api.supermercato24.it/sm/api/v3/locations/$LOCATIONID/stores/$STOREID/availability?funnel=POSTAL_CODE_POPUP' \ "
+	#echo "-H 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0' \ "
+	#echo "-H 'Accept: application/json, text/plain, */*' \ "
+	#echo "-H 'Accept-Language: it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3' \ "
+	#echo "-H 'Referer: https://www.supermercato24.it/s' \ "
+	#echo "-H 'Authorization: Bearer $BEARERID' \ "
+	#echo "-H 'X-S24-Client: website/2.0.0-alpha.1' \ "
+	#echo "-H 'X-S24-Country: ITA' \ "
+	#echo "-H 'Origin: https://www.supermercato24.it' \ "
+	#echo "-H 'Connection: keep-alive' \ "
+	#echo "-H 'Pragma: no-cache' \ "
+	#echo "-H 'Cache-Control: no-cache' \ "
+	#echo "-w '\nHTTP_STATUS: %{http_code}\n' \ "
+	#echo "--compressed \ "
+	#echo "	2> >(tee -a $TEMP_ERR) 1> >(tee -a $TEMP_CHK)"
+	#
 	echo "curl 'https://api.supermercato24.it/sm/api/v3/locations/$LOCATIONID/stores/$STOREID/availability?funnel=POSTAL_CODE_POPUP' \ "
-	echo "-H 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0' \ "
-	echo "-H 'Accept: application/json, text/plain, */*' \ "
-	echo "-H 'Accept-Language: it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3' \ "
-	echo "-H 'Referer: https://www.supermercato24.it/s' \ "
-	echo "-H 'Authorization: Bearer $BEARERID' \ "
-	echo "-H 'X-S24-Client: website/2.0.0-alpha.1' \ "
-	echo "-H 'X-S24-Country: ITA' \ "
-	echo "-H 'Origin: https://www.supermercato24.it' \ "
-	echo "-H 'Connection: keep-alive' \ "
-	echo "-H 'Pragma: no-cache' \ "
-	echo "-H 'Cache-Control: no-cache' \ "
-	echo "-w '\nHTTP_STATUS: %{http_code}\n' \ "
-	echo "--compressed \ "
+	echo "-H 'Connection: keep-alive' \                                                                                      "
+	echo "-H 'X-S24-Country: ITA' \                                                                                          "
+	echo "-H 'X-S24-Client: website/2.0.0-alpha.1' \                                                                         "
+	echo "-H 'Origin: https://www.supermercato24.it' \                                                                       "
+	echo "-H 'Authorization: Bearer 8892de5e7ba5158e3ef52c1db2bfabcc8f4e86de' \                                              "
+	echo "-H 'Accept: application/json, text/plain, */*' \                                                                   "
+	echo "-H 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0' \                   "
+	echo "-H 'Referer: https://www.supermercato24.it/s' \                                                                    "
+	echo "--compressed \                                                                                                     "
+	echo "-w '\nHTTP_STATUS: %{http_code}\n' \                                                                                 "
 	echo "	2> >(tee -a $TEMP_ERR) 1> >(tee -a $TEMP_CHK)"
 	#
 	echo "########################################################################################################"
@@ -84,19 +101,32 @@ function firefoxcheck()
 	#	run command
 	########################################################################################################
 	#OUTPUT="$(
-	$DEBUG \
+	# $DEBUG \
+	# curl "https://api.supermercato24.it/sm/api/v3/locations/$LOCATIONID/stores/$STOREID/availability?funnel=POSTAL_CODE_POPUP" \
+	# -H 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0' \
+	# -H 'Accept: application/json, text/plain, */*' \
+	# -H 'Accept-Language: it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3' \
+	# -H 'Referer: https://www.supermercato24.it/s' \
+	# -H "Authorization: Bearer $BEARERID" \
+	# -H 'X-S24-Client: website/2.0.0-alpha.1' \
+	# -H 'X-S24-Country: ITA' \
+	# -H 'Origin: https://www.supermercato24.it' \
+	# -H 'Connection: keep-alive' \
+	# -H 'Pragma: no-cache' \
+	# -H 'Cache-Control: no-cache' \
+	# -w '\nHTTP_STATUS: %{http_code}\n' \
+	# --compressed \
+	# 2> >(tee -a $TEMP_ERR) 1> >(tee -a $TEMP_CHK)
+	#$DEBUG
 	curl "https://api.supermercato24.it/sm/api/v3/locations/$LOCATIONID/stores/$STOREID/availability?funnel=POSTAL_CODE_POPUP" \
-	-H 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0' \
-	-H 'Accept: application/json, text/plain, */*' \
-	-H 'Accept-Language: it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3' \
-	-H 'Referer: https://www.supermercato24.it/s' \
-	-H "Authorization: Bearer $BEARERID" \
-	-H 'X-S24-Client: website/2.0.0-alpha.1' \
-	-H 'X-S24-Country: ITA' \
-	-H 'Origin: https://www.supermercato24.it' \
 	-H 'Connection: keep-alive' \
-	-H 'Pragma: no-cache' \
-	-H 'Cache-Control: no-cache' \
+	-H 'X-S24-Country: ITA' \
+	-H 'X-S24-Client: website/2.0.0-alpha.1' \
+	-H 'Origin: https://www.supermercato24.it' \
+	-H 'Authorization: Bearer 8892de5e7ba5158e3ef52c1db2bfabcc8f4e86de' \
+	-H 'Accept: application/json, text/plain, */*' \
+	-H 'User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0' \
+	-H 'Referer: https://www.supermercato24.it/s' \
 	-w '\nHTTP_STATUS: %{http_code}\n' \
 	--compressed \
 	2> >(tee -a $TEMP_ERR) 1> >(tee -a $TEMP_CHK)
@@ -130,8 +160,14 @@ function firefoxcheck()
 
 	if [ "$STATUSCODE" -eq 404 ]
 	then
-		echo "NON CI SONO DISPONIBILITA'"
-		#return
+		echo "$STORENAME"
+		echo "$STOREADDRESS"
+		echo "########################################################################################################"
+		echo "#                                        NESSUNA DISPONIBILITA'                                      #"
+		echo "#                                                                                                      #"
+		echo "					$STORENAME"
+		echo "					$STOREADDRESS"
+		echo "########################################################################################################"
 	elif [ $STATUSCODE -eq 200 ]
 	then
 		echo "$STORENAME"
