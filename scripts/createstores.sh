d69d9e385e0e35e78f541556b57440c9acd246ce
atrox3d <atrox3d@gmail.com>
2020-03-27 11:01:01 +0100

    organize code

--- scripts/createstores.sh
+++ scripts/createstores.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-
 # ./jq.sh ../data/***REMOVED***\@***REMOVED***/json/***REMOVED***.STORES.opera.json | while read -r a b c d; do echo -e "$a\t$b\t$c\t$d"; done
-./jq.sh ../data/***REMOVED***\@***REMOVED***/json/***REMOVED***.STORES.opera.json | while read -r a b c d; do echo -e "$a\t$b\t$c\t$d"; done
+################################################################################
+#
+#	load include
+#
+################################################################################
+. functions.include
+
+[ $# -ge 1 ] || { echo "syntax $BASH_SOURCE email"; exit 1; }
+
+EMAIL="$1"
+
+
+DATADIR=../data/$EMAIL
+JSONDIR="${DATADIR}/json"
+DATAFILE="${JSONDIR}/stores.json"
+
+./jq.sh "$DATAFILE" | while read -r a b c d; do echo -e "$a\t$b\t$c\t$d"; done
