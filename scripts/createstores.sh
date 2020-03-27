4460937eb6292823eee1383b9c61a63dc461cb4c
atrox3d <atrox3d@gmail.com>
2020-03-27 10:51:10 +0100

    createstores.sh

---
+++ scripts/createstores.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# ./jq.sh ../data/***REMOVED***\@***REMOVED***/json/***REMOVED***.STORES.opera.json | while read -r a b c d; do echo -e "$a\t$b\t$c\t$d"; done
+./jq.sh ../data/***REMOVED***\@***REMOVED***/json/***REMOVED***.STORES.opera.json | while read -r a b c d; do echo -e "$a\t$b\t$c\t$d"; done
