781295a1a9aac2cb5411da40d59a1687ffe2386d
atrox3d <atrox3d@gmail.com>
2020-03-27 09:19:50 +0100

    add csv output

--- scripts/jq.sh
+++ scripts/jq.sh
@@ -1,29 +1,67 @@
 #!/bin/bash
-#jq -c '.data.body[] | select(.widget_type | contains("vertical-list")).list[] | {storeID:.id, storeName:.name, locationID:.tracking[].data.location_id, storeAddress:.tracking[].data.store_address}' ***REMOVED***.stores.opera.json
 
+#./jq.sh ../opera/out/***REMOVED***.stores.opera.json \
+#	| while read -r a b c d
+#		do echo "$c, $d
+#	done
+
+################################################################################
+#
+#	load include
+#
+################################################################################
 . functions.include
-
-JSONFILE=../opera/out/***REMOVED***.stores.opera.json
-
+########################################################################################################
+# check parameters or exit
+########################################################################################################
+[ $# -ge 1 ] || {
+	echo "syntax $(basename $BASH_SOURCE) filename [csv]"
+	exit 1
+}
+CSV=$2
+################################################################################
+#
+#	OS aware filename
+#
+################################################################################
+#JSONFILE=../opera/out/***REMOVED***.stores.opera.json
+JSONFILE="$1"
 if iswindows
 then
 	DIRNAME="$(dirname "$JSONFILE")"
 	JSONFILE="$(basename "$JSONFILE")"
-	pushd "$DIRNAME"
+	pushd "$DIRNAME" > /dev/null
 fi
-
-jq -c \
+################################################################################
+#
+#	extract data
+#
+################################################################################
+JQOUT="$(
+jq -rc \
 '.data.body[]'\
 '| select(.widget_type | contains("vertical-list")).list[]'\
 '| {
 		storeID:.id,
-		storeName:.name,
 		locationID:.tracking[].data.location_id,
+		storeName:.name,
 		storeAddress:.tracking[].data.store_address
 }' \
 "$JSONFILE"
+)"
+
+[ -v CSV ] && {
+	#echo "$JQOUT" |  jq -rc "[ .storeID, .storeName, .locationID, .storeAddress ] | @csv"
+	#echo "$JQOUT" |  jq -c '[ .storeID, .storeName, .locationID, .storeAddress ]' 
+	#| jq -rc '.[0] + " " + .[1]'
+	#echo "$JQOUT" |  jq -cr '. | "\(.storeID) \(.storeName) \(.locationID) '\''\(.storeAddress)'\''"'
+	echo "$JQOUT" |  jq -cr '. | "\(.storeID) \(.locationID) \(.storeName) \(.storeAddress)"'
+} || {
+	echo "$JQOUT"
+}
+
 
 if iswindows
 then
-	popd
+	popd  > /dev/null
 fi
