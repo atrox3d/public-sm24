b61bc091fedba055bc12ce639856be2ce96423e7
atrox3d <atrox3d@gmail.com>
2020-03-30 06:11:29 +0200

    change mail subject, address - SM24 - storename

--- scripts/firefox.check.sh
+++ scripts/firefox.check.sh
@@ -226,7 +226,7 @@ function firefoxcheck()
 		echo "########################################################################################################"
 		echo "# INVIO MAIL A : $MAILADDRESS"
 		echo "########################################################################################################"
-		./mailer.sh "$MAILADDRESS" "$MAILADDRESS - SLOT DISPONIBILE PER $STORENAME" "TROVATO SLOT PER $STORENAME - $STOREADDRESS"
+		./mailer.sh "$MAILADDRESS" "$MAILADDRESS - SM24 - $STORENAME" "TROVATO SLOT PER $STORENAME - $STOREADDRESS"
 		if PARSER="$(getJSONparser)"
 		then
 			echo "found JSON parser : $PARSER"
