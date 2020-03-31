e77cbd8751cc92ec8a5e7c0d7edb643f4a02274c
atrox3d <atrox3d@gmail.com>
2020-03-31 11:15:19 +0200

    json2mail.sh

---
+++ scripts/json2mail.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+################################################################################
+#                                                                              #
+#	START                                                                      #
+#                                                                              #
+################################################################################
+STARTPATH="$(pwd)"
+SCRIPTPATH="$(dirname $(realpath $0))"
+cd "$SCRIPTPATH"
+SCRIPTNAME="$(basename $0)"
+INCLUDE="${SCRIPTPATH}/functions.include"
+################################################################################
+#                                                                              #
+#	load include                                                               #
+#                                                                              #
+################################################################################
+. "$INCLUDE" || { echo "ERROR|cannot source $INCLUDE"; exit 1; }
+################################################################################
+checkparameters $# 3 "$(basename $BASH_SOURCE) OUTDIR EMAIL TIMESERIAL"
+OUTDIR="$1"
+EMAIL="$2"
+TIMESERIAL="$3"
+PATTERN="${OUTDIR}"/"${EMAIL}".*.200."${TIMESERIAL}".json
+MAILPATTERN="${OUTDIR}"/"${EMAIL}"."${TIMESERIAL}".mail
+
+echo "INFO| OUTDIR    : $OUTDIR"
+echo "INFO| EMAIL     : $EMAIL"
+echo "INFO| TIMESERIAL: $TIMESERIAL"
+echo "INFO| PATTERN   ; $PATTERN"
+
+for file in $PATTERN
+do
+	[ -f "$file" ] || continue
+	#echo "DEBUG| $file" >&2
+	#
+	#	extract store
+	#
+	filename="$(basename $file)"
+	#echo "$filename"
+	IFS=. read -r mail domain STORE status date timestamp <<< "$filename"
+	echo $STORE
+	echo
+	cat $file | jq '.data.data[].hours[] | .label'
+	echo
+done > "$MAILPATTERN"
+
+cat "$MAILPATTERN"
