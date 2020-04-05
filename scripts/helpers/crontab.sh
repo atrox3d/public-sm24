e4b43314daee38526fe8e8ec6e60018ae73f5255
atrox3d <atrox3d@gmail.com>
2020-04-05 10:13:08 +0200

    fix paths

--- scripts/helpers/crontab.sh
+++ scripts/helpers/crontab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPTPATH="$(dirname $(realpath $0))"
-INCLUDE="${SCRIPTPATH}/include/functions.include"
-LOGDIR="${SCRIPTPATH}/log"
+INCLUDE="${SCRIPTPATH}/../include/functions.include"
+LOGDIR="${SCRIPTPATH}/../log"
 
 . "$INCLUDE"
 echo "$(timestamp) - $LOGDIR" 2>&1 | tee -a "${LOGDIR}/crontab.log"
